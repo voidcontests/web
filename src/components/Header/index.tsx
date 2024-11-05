dcf37868f383e0e17adefe47a2e04507c735a6f1
jus1d <artfa63@gmail.com>
2024-11-06 01:43:38 +0400

    chore: Add `header` component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { TonConnectButton } from "@tonconnect/ui-react";
+import styles from "./styles.module.css";
+import Link from "next/link";
+
+export const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <Link href={"/"} className={styles.title}>CIIM</Link>
+                <div className={styles.right}>
+                    <Link href={"/contests"} className={styles.link}>Contests</Link>
+                    <Link href={"/leaderboards"} className={styles.link}>Leaderboards</Link>
+                    <TonConnectButton />
+                </div>
+            </div>
+            <hr className={styles.separator} />
+        </header>
+    );
+}
