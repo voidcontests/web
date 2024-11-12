cd9673312dc1d0aa4191eb600efee561ad519c25
jus1d <artfa63@gmail.com>
2024-11-12 23:08:16 +0400

    chore: Add card styling

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import RightArrow from "@/icons/RightArrow";
 import styles from "./styles.module.css";
 
 interface Props {
@@ -13,16 +14,33 @@ interface Props {
 export const Card = (props: Props) => {
     return (
         <div className={styles.card}>
-            {props.icon}
-            <h1>
-                {props.title}
-            </h1>
-            <p>
-                {props.description}
-            </p>
-            <a href={props.link}>
-                {props.linkLabel}
-            </a>
+            <div className={styles.container}>
+                {props.icon}
+                <div className={styles.title}>
+                    {props.title}
+                </div>
+                <div className={styles.description}>
+                    {props.description}
+                </div>
+
+                <a href={props.link} className={styles.link}>
+                    <div className={styles.label}>
+                        {props.linkLabel}
+                    </div>
+                    <RightArrow className={styles.arrow} />
+                </a>
+            </div>
+
+            {/* <div className={styles.icon}>
+            </div>
+            <div className={styles.text}>
+                <h1 className={styles.title}>
+                </h1>
+                <p className={styles.description}>
+                </p>
+            </div>
+            <div className={styles.linkContainer}>
+            </div> */}
         </div>
     );
 }
