5544a9d7e7f4c07fa776bd20c1905c69a1058b02
jus1d <artfa63@gmail.com>
2024-11-13 14:28:34 +0400

    chore: Add styles to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,17 @@
 export default function NotFound() {
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            NOT FOUND
-        </div>
+            <div>
+                <div style={{ fontWeight: "700", fontSize: "130px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    404
+                </div>
+                <hr style={{
+                    color: "#bcbcbc", height: "1px", marginTop: "0", border: "none", backgroundColor: "var(--gray)",
+                }} />
+                <div style={{ fontSize: "22px" }}>
+                    Where did you get this link?
+                </div>
+            </div>
+        </div >
     );
 }
