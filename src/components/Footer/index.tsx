b4c9302cf3b5362fdfb93a2c27ac04f663221bb4
jus1d <artfa63@gmail.com>
2024-11-15 12:45:22 +0400

    chore: Add adjustable colors for footer icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,4 @@
-import LinkedIn from "@/icons/LinkedIn";
 import GitHub from "@/icons/GitHub";
-import Twitter from "@/icons/Twitter";
 import Email from "@/icons/Email";
 import styles from "./styles.module.css";
 import Link from "next/link";
@@ -14,9 +12,7 @@ export const Footer = () => {
                     Created by <Link href="https://github.com/jus1d" className={styles.link}>@ndbtea</Link>
                 </div>
                 <div className={styles.icons}>
-                    <LinkedIn />
                     <GitHub />
-                    <Twitter />
                     <Email />
                 </div>
             </div>
