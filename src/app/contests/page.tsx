dd24336422aa1ff1c90987efd1e60263b984910e
jus1d <artfa63@gmail.com>
2025-01-04 01:43:13 +0400

    chore: Add official contests cards

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -10,6 +10,11 @@ import {
   TableHeaderRow,
   TableHeaderCell,
 } from "@/components/ui/table";
+import {
+  Card, CardContent, CardTitle, CardFooter
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 interface Contest {
   title: string,
@@ -92,6 +97,65 @@ export default function ContestsPage() {
     <div className="flex justify-center">
       <div className="w-[1200px] flex gap-[20px] m-[40px]">
         <div className="w-full flex flex-col gap-[30px]">
+          <div className="flex flex-col gap-[10px]">
+            <h1 className="text-[20px] font-medium">OFFICIAL CONTESTS</h1>
+            <div className="flex gap-[30px]">
+              <Card className="w-[300px]">
+                <CardContent>
+                  <div>
+                    <Badge variant="green">Onboarding</Badge>
+                  </div>
+                  <CardTitle>
+                    Introduction
+                  </CardTitle>
+                  <Separator />
+                  <div className="flex justify-between">
+                    <div>Prize pot</div>
+                    <div>69 TON</div>
+                  </div>
+                  <div className="flex justify-between">
+                    <div>Entry price</div>
+                    <div>0.5 TON</div>
+                  </div>
+                  <Separator />
+                  <div>
+                    Hosted by <Link href='https://github.com/jus1d'>@ndbtea</Link>
+                  </div>
+                </CardContent>
+                <CardFooter className="pt-[20px]">
+                  <Link href="/hui" size="large">LEADERBOARD</Link>
+                </CardFooter>
+              </Card>
+
+              <Card className="w-[300px]">
+                <CardContent>
+                  <div>
+                    <Badge>Starting soon</Badge>
+                  </div>
+                  <CardTitle>
+                    The Void Round 297
+                  </CardTitle>
+                  <Separator />
+                  <div className="flex justify-between">
+                    <div>Prize pot</div>
+                    <div>117 TON</div>
+                  </div>
+                  <div className="flex justify-between">
+                    <div>Entry price</div>
+                    <div>1 TON</div>
+                  </div>
+                  <Separator />
+                  <div>
+                    Hosted by <Link href='https://github.com/jus1d'>@ndbtea</Link>
+                  </div>
+                </CardContent>
+                <CardFooter className="pt-[20px]">
+                  <Button variant="link" className="w-full">APPLY</Button>
+                </CardFooter>
+              </Card>
+            </div>
+          </div>
+          <Separator />
           <TableContainer>
             <TableHead>
               PUBLIC COMPETITIONS
