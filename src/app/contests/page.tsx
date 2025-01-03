1a4781e975bdd20492a7d39cf9f3ba036fa33041
jus1d <artfa63@gmail.com>
2025-01-04 01:34:45 +0400

    chore: Add past competitions table

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -16,13 +16,45 @@ interface Contest {
   host_username: string,
   host_url: string,
   type: 'paid' | 'training',
-  entry_price_tons: number,
+  participants?: number,
+  entry_price_tons?: number,
   prize_pot_tons: number,
-  start: string,
-  duration: string,
+  start?: string,
+  finish?: string,
+  duration?: string,
 }
 
-const contests: Contest[] = [
+const pastContests: Contest[] = [
+  {
+    title: 'canary-contest',
+    host_username: 'ndbtea',
+    host_url: 'https://github.com/jus1d',
+    type: 'training',
+    participants: 291,
+    prize_pot_tons: 0,
+    finish: '21 Jan, 7 PM',
+  },
+  {
+    title: 'La Guerra de Marmotas',
+    host_username: 'reilix',
+    host_url: 'https://github.com/Re1l1x',
+    type: 'paid',
+    participants: 2100,
+    prize_pot_tons: 3150,
+    finish: '28 May, 2 PM',
+  },
+  {
+    title: 'VOID Round 296',
+    host_username: 'ccc',
+    host_url: 'https://github.com/jus1d',
+    type: 'paid',
+    participants: 97,
+    prize_pot_tons: 485,
+    finish: '24 Nov, 11 PM',
+  },
+];
+
+const publicContests: Contest[] = [
   {
     title: 'canary-contest',
     host_username: 'ndbtea',
@@ -34,7 +66,7 @@ const contests: Contest[] = [
     duration: '3h',
   },
   {
-    title: 'La Guerra De Marmotas',
+    title: 'La Guerra de Marmotas',
     host_username: 'reilix',
     host_url: 'https://github.com/Re1l1x',
     type: 'paid',
@@ -59,51 +91,99 @@ export default function ContestsPage() {
   return (
     <div className="flex justify-center">
       <div className="w-[1200px] flex gap-[20px] m-[40px]">
-        <TableContainer>
-          <TableHead>
-            PUBLIC COMPETITIONS
-          </TableHead>
-          <Table>
-            <TableHeaderRow>
-              <TableRow>
-                <TableHeaderCell className="ml-4">#</TableHeaderCell>
-                <TableHeaderCell>Title</TableHeaderCell>
-                <TableHeaderCell>Host</TableHeaderCell>
-                <TableHeaderCell>Type</TableHeaderCell>
-                <TableHeaderCell>Entry price</TableHeaderCell>
-                <TableHeaderCell>Prize pot</TableHeaderCell>
-                <TableHeaderCell>Start</TableHeaderCell>
-                <TableHeaderCell>Duration</TableHeaderCell>
-              </TableRow>
-            </TableHeaderRow>
-            <TableBody>
-              {
-                contests.map((contest, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{`${index}/`}</TableCell>
-                    <TableCell>{contest.title}</TableCell>
-                    <TableCell>
-                      <Link href={contest.host_url}>{`@${contest.host_username}`}</Link>
-                    </TableCell>
-                    <TableCell>
-                      {
-                        contest.type == "paid" ?
-                          <Badge variant="green">Paid</Badge> :
-                          <Badge variant="secondary">Training</Badge>
-                      }
-                    </TableCell>
-                    <TableCell>{contest.entry_price_tons} TON</TableCell>
-                    <TableCell>{contest.prize_pot_tons} TON</TableCell>
-                    <TableCell>{contest.start}</TableCell>
-                    <TableCell>{contest.duration}</TableCell>
-                  </TableRow>
-                ))
-              }
-            </TableBody>
-          </Table>
-          {/* <TableFooter>Subscribe @ndbtea</TableFooter> */}
-        </TableContainer>
+        <div className="w-full flex flex-col gap-[30px]">
+          <TableContainer>
+            <TableHead>
+              PUBLIC COMPETITIONS
+            </TableHead>
+            <Table>
+              <TableHeaderRow>
+                <TableRow>
+                  <TableHeaderCell>#</TableHeaderCell>
+                  <TableHeaderCell>Title</TableHeaderCell>
+                  <TableHeaderCell>Host</TableHeaderCell>
+                  <TableHeaderCell>Type</TableHeaderCell>
+                  <TableHeaderCell>Entry price</TableHeaderCell>
+                  <TableHeaderCell>Prize pot</TableHeaderCell>
+                  <TableHeaderCell>Start</TableHeaderCell>
+                  <TableHeaderCell>Duration</TableHeaderCell>
+                </TableRow>
+              </TableHeaderRow>
+              <TableBody>
+                {
+                  publicContests.map((contest, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{`${index}/`}</TableCell>
+                      <TableCell>{contest.title}</TableCell>
+                      <TableCell>
+                        <Link href={contest.host_url}>{`@${contest.host_username}`}</Link>
+                      </TableCell>
+                      <TableCell>
+                        {
+                          contest.type == "paid" ?
+                            <Badge variant="green">Paid</Badge> :
+                            <Badge variant="secondary">Training</Badge>
+                        }
+                      </TableCell>
+                      <TableCell>{contest.entry_price_tons} TON</TableCell>
+                      <TableCell>{contest.prize_pot_tons} TON</TableCell>
+                      <TableCell>{contest.start}</TableCell>
+                      <TableCell>{contest.duration}</TableCell>
+                    </TableRow>
+                  ))
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TableContainer>
+            <TableHead>
+              PAST COMPETITIONS
+            </TableHead>
+            <Table>
+              <TableHeaderRow>
+                <TableRow>
+                  <TableHeaderCell>#</TableHeaderCell>
+                  <TableHeaderCell>Title</TableHeaderCell>
+                  <TableHeaderCell>Host</TableHeaderCell>
+                  <TableHeaderCell>Type</TableHeaderCell>
+                  <TableHeaderCell>Participants</TableHeaderCell>
+                  <TableHeaderCell>Prize pot</TableHeaderCell>
+                  <TableHeaderCell>Finished</TableHeaderCell>
+                  <TableHeaderCell></TableHeaderCell>
+                </TableRow>
+              </TableHeaderRow>
+              <TableBody>
+                {
+                  pastContests.map((contest, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{`${index}/`}</TableCell>
+                      <TableCell>{contest.title}</TableCell>
+                      <TableCell>
+                        <Link href={contest.host_url}>{`@${contest.host_username}`}</Link>
+                      </TableCell>
+                      <TableCell>
+                        {
+                          contest.type == "paid" ?
+                            <Badge variant="green">Paid</Badge> :
+                            <Badge variant="secondary">Training</Badge>
+                        }
+                      </TableCell>
+                      <TableCell>{contest.participants} TON</TableCell>
+                      <TableCell>{contest.prize_pot_tons} TON</TableCell>
+                      <TableCell>{contest.finish}</TableCell>
+                      <TableCell>
+                        <Link href={`/contest/${index}/scoring`} size="large">
+                          SCORING
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
-    </div >
+    </div>
   );
 }
