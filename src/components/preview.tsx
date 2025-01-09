ebdeb8d5a23af5f57c0c5af2b0879ee999701725
jus1d <artfa63@gmail.com>
2025-01-09 12:53:58 +0400

    fix: Remove unused colors + fix i dont remember what

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -18,7 +18,7 @@ const Preview = React.forwardRef<HTMLDivElement, PreviewProps>(({ markdown, clas
             className={cn(
                 'prose prose-invert',
                 'prose-headings:font-normal',
-                'prose-code:bg-background-secondary-subdued prose-code:p-1 prose-code:rounded-lg',
+                'prose-code:bg-background-secondary prose-code:border prose-code:p-1 prose-code:rounded-lg',
                 'prose-a:text-text-link prose-a:no-underline hover:prose-a:underline underline-offset-2 prose-a:font-normal',
                 'prose-hr:border-border-primary prose-hr:w-full',
                 'prose-strong:font-bold',
