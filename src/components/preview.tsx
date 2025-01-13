9e0e737d5712d3aab566b0aa97f137605671f907
jus1d <artfa63@gmail.com>
2025-01-13 21:03:13 +0400

    chore: change markdown from bold to medium

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -21,7 +21,7 @@ const Preview = React.forwardRef<HTMLDivElement, PreviewProps>(({ markdown, clas
                 'prose-code:bg-background-secondary prose-code:border prose-code:p-1 prose-code:rounded-lg',
                 'prose-a:text-text-link prose-a:no-underline hover:prose-a:underline underline-offset-2 prose-a:font-normal',
                 'prose-hr:border-border-primary prose-hr:w-full',
-                'prose-strong:font-bold',
+                'prose-strong:font-medium',
                 'prose-img:rounded',
                 'codeblock',
                 className,
