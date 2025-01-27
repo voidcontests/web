ad97575186ca1b9cb0080fb93df03f1bbb12fbdb
jus1d <artfa63@gmail.com>
2025-01-27 15:07:53 +0400

    chore: cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import { TonConnectProvider } from "@/components/ton-connect/provider";
-import { Notification } from "@/components/notification";
 import { ThemeProvider } from "@/contexts/ThemeContext";
-import { ChevronRight } from "lucide-react";
 import { Footer } from "@/components/footer";
 import Header from "@/components/header";
 import { cookies } from "next/headers";
@@ -15,20 +13,14 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  const theme = cookies().get('theme')?.value === 'light' ? 'light' : 'dark';
+  const cookieStore = cookies();
+  const theme = cookieStore.get('theme')?.value === 'light' ? 'light' : 'dark';
 
   return (
     <html lang="en">
       <ThemeProvider>
         <TonConnectProvider>
           <body className={`flex flex-col min-h-dvh antialiased ${theme}`}>
-            {/* <Notification
-              variant="default"
-              href="https://github.com/voidcontests/frontend/issues/new?assignees=&labels=&projects=&template=bug_report.md&title="
-            >
-              <div>This is an early dev build. Report bugs here</div>
-              <ChevronRight className="w-5 h-5" />
-            </Notification> */}
             <Header />
             <main className="flex-grow">
               {children}
