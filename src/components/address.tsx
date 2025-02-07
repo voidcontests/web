140ba69ff50b93c871bf0dd133bfe5cdf3b93816
jus1d <artfa63@gmail.com>
2025-02-07 14:08:59 +0400

    chore: Use user-friendly address

--- src/components/address.tsx
+++ src/components/address.tsx
@@ -1,16 +1,19 @@
-import { Link } from "@/components/ui/link";
 import { truncate_address } from "@/lib/strings";
+import { Link } from "@/components/ui/link";
+import { Address as Addr } from "@ton/core";
 
 const EXPLORER_BASEPATH = "https://tonscan.org";
 
 export default function Address({ address }: { address: string }) {
+    const addr = Addr.parse(address).toString({ bounceable: false });
+
     return (
         <Link
-            href={`${EXPLORER_BASEPATH}/address/${address}`}
+            href={`${EXPLORER_BASEPATH}/address/${addr}`}
             target="_blank"
             rel="noopener noreferrer"
         >
-            {truncate_address(address, 8)}
+            {truncate_address(addr, 12)}
         </Link>
     );
 }
