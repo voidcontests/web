3a4ceea34ad18e8ec9c734e03ebe8c9947126d8a
jus1d <artfa63@gmail.com>
2025-03-08 22:48:15 +0400

    chore: update problem skeleton

--- src/components/sections/loading.tsx
+++ src/components/sections/loading.tsx
@@ -22,15 +22,25 @@ export function ProblemTemplate() {
             <div className='flex justify-center items-center'>
                 <Skeleton className='h-6 w-96'/>
             </div>
-            <div className='flex flex-col gap-5'>
+            <div className='flex flex-col gap-8'>
                 <div className='flex flex-col gap-3'>
-                    <Skeleton className='h-4 w-[100%]'/>
-                    <Skeleton className='h-4 w-[97%]'/>
-                    <Skeleton className='h-4 w-[90%]'/>
+                    <div className='pl-4'>
+                        <Skeleton className='h-4 w-full'/>
+                    </div>
+                    <Skeleton className='h-4 w-full'/>
+                    <Skeleton className='h-4 w-9/10'/>
                 </div>
                 <div className='flex flex-col gap-3'>
-                    <Skeleton className='h-4 w-[100%]'/>
-                    <Skeleton className='h-4 w-[72%]'/>
+                    <div className='pl-4'>
+                        <Skeleton className='h-4 w-full'/>
+                    </div>
+                    <Skeleton className='h-4 w-9/12'/>
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <Skeleton className='h-4 w-[18%]'/>
+                    <Skeleton className='ml-[3%] h-4 w-[43%]'/>
+                    <Skeleton className='ml-[3%] h-4 w-[27%]'/>
+                    <Skeleton className='ml-[3%] h-4 w-[33%]'/>
                 </div>
             </div>
         </div>
