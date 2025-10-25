f49b604f0e1c29d86d13ec19f03c86c0a79e2f2b
jus1d <artfa63@gmail.com>
2025-10-25 14:27:47 +0400

    chore: Add problem preview

--- src/modules/problem/statement.tsx
+++ src/modules/problem/statement.tsx
@@ -1,15 +1,28 @@
 import Preview from "@/components/preview";
 import { Container } from "@/containers/default";
-import { ContestProblemDetailed } from "@/lib/models";
+import { ContestProblemDetailed, ProblemDetailed } from "@/lib/models";
+import { ContestProblemDetailedSchema } from "@/lib/schemas";
 import { Separator } from "@/ui/separator";
 
-export default function Statement({ problem }: { problem: ContestProblemDetailed }) {
+type StatementProps = {
+    problem: ContestProblemDetailed | ProblemDetailed;
+};
+
+function isContestProblem(problem: ContestProblemDetailed | ProblemDetailed): problem is ContestProblemDetailed {
+    return ContestProblemDetailedSchema.safeParse(problem).success;
+}
+
+export default function Statement({ problem }: StatementProps) {
     const points = problem.difficulty === 'easy' ? 1 : problem.difficulty === 'mid' ? 2 : 3;
+    const title = isContestProblem(problem)
+        ? `${problem.charcode}/ ${problem.title}`
+        : `${problem.id}/ ${problem.title}`;
+
     return (
         <Container className="py-5 px-7">
             <div className="flex justify-center items-center gap-3 text-center">
                 <span className="text-lg">
-                    {`${problem.charcode}/ ${problem.title}`}
+                    {title}
                 </span>
                 <span className="text-lg text-secondary-foreground">
                     {`(${points} pts.)`}
