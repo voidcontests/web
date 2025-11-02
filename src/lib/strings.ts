a74cb58530947c0f32dd1887fc2d9794c29fca6d
jus1d <artfa63@gmail.com>
2025-11-02 04:10:56 +0400

    fix: capitalize: do not change rest of the word, except first char

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -27,5 +27,5 @@ export const getLineStartIndex = (text: string, index: number): number => {
 
 export const capitalize = (word: string): string => {
     if (!word) return '';
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    return word.charAt(0).toUpperCase() + word.slice(1);
 }
