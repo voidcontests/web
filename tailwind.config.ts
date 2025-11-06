93a9ac28ce81f350d6d4db958dd87b09df8a7816
jus1d <artfa63@gmail.com>
2025-11-06 14:25:10 +0400

    chore: Introduce styrene font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
 	theme: {
+		extend: {
+			fontFamily: {
+				'styrene-a': ['"Styrene A"', 'sans-serif'],
+				'styrene-b': ['"Styrene B"', 'sans-serif'],
+				'styrene': ['"Styrene A"', 'sans-serif'], // Default to Styrene A
+			},
+		},
 		screens: {
 			"2xl": { max: "1535px" },
 			xl: { max: "1279px" },
